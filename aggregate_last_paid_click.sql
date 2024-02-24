7391fe7c62c4f6ed292f79915e7124faf7df7ce4
Doomshap <152163342+Doomshap@users.noreply.github.com>
2024-02-24 13:44:47 +1200

    Add files via upload

---
+++ aggregate_last_paid_click.sql
@@ -0,0 +1,44 @@
+with tab1 as(
+select distinct on (sessions.visitor_id)
+sessions.visitor_id, sessions.visit_date, leads.created_at, leads.status_id, amount, lead_id, closing_reason, medium, source, campaign 
+from sessions 
+left join leads 
+on sessions.visitor_id = leads.visitor_id
+and sessions.visit_date <= leads.created_at
+where medium != 'organic'
+order by sessions.visitor_id, visit_date desc
+),
+
+tab as
+(select utm_source, utm_medium, utm_campaign, cast(campaign_date as date) as campaign_date,
+sum(daily_spent) as total_cost
+from vk_ads
+group by 1,2,3,4
+union
+select utm_source, utm_medium, utm_campaign,cast(campaign_date as date) as campaign_date,
+sum(daily_spent) as total_cost
+from ya_ads
+group by 1,2,3,4),
+
+ tab2 as
+(select source, medium, campaign, cast(visit_date as date) as visit_date,
+count (visitor_id) as visitors_count,
+count (visitor_id) filter(where tab1.created_at is not null)as leads_count,
+count (visitor_id) filter(where tab1.status_id = 142) as purchases_count,
+sum(amount) filter(where tab1.status_id = 142) as revenue
+from tab1
+group by source, medium, campaign, cast(visit_date as date))
+
+select to_char(visit_date, 'yyyy-mm-dd') as visit_date,
+visitors_count, tab2.source as utm_source, tab2.medium as utm_medium, tab2.campaign as  utm_campaign,
+total_cost,leads_count, purchases_count, revenue
+from  tab2
+left join tab
+on tab2.medium = tab.utm_medium and 
+tab2.source = tab.utm_source and 
+tab2.campaign = tab.utm_campaign and 
+tab2.visit_date = tab.campaign_date
+where tab2.medium != 'organic'
+order by 9 desc nulls last, 1 asc, visitors_count desc, utm_source asc, utm_medium asc, utm_campaign asc
+limit 15
+
