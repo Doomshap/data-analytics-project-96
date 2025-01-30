62e71cdd435a50312912cd6743fe8e77e948b47d
Doomshap <152163342+Doomshap@users.noreply.github.com>
2025-01-30 17:17:32 +1200

    Update aggregate_last_paid_click.sql

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -1,63 +1,71 @@
 WITH tab1 AS (
-        SELECT DISTINCT ON (sessions.visitor_id)
-                sessions.visitor_id,
-                sessions.visit_date,
-                leads.created_at,
-                leads.status_id,
-                leads.amount,
-                leads.lead_id,
-                leads.closing_reason,
-                sessions.medium,
-                sessions.source,
-                sessions.campaign
-        FROM sessions
-        LEFT JOIN leads
-            ON sessions.visitor_id = leads.visitor_id
-            AND sessions.visit_date <= leads.created_at
-        WHERE sessions.medium != 'organic'
-        ORDER BY
-            sessions.visitor_id ASC,
-            sessions.visit_date DESC
+    SELECT DISTINCT ON (sessions.visitor_id)
+        sessions.visitor_id,
+        sessions.visit_date,
+        leads.created_at,
+        leads.status_id,
+        leads.amount,
+        leads.lead_id,
+        leads.closing_reason,
+        sessions.medium,
+        sessions.source,
+        sessions.campaign
+    FROM sessions
+    LEFT JOIN leads
+        ON sessions.visitor_id = leads.visitor_id
+        AND sessions.visit_date <= leads.created_at
+    WHERE sessions.medium != 'organic'
+    ORDER BY
+        sessions.visitor_id ASC,
+        sessions.visit_date DESC
 ),
 
-    tab AS (
-        SELECT
-            utm_source,
-            utm_medium,
-            utm_campaign,
-            CAST(campaign_date AS DATE) AS campaign_date,
-            SUM(daily_spent) AS total_cost
-        FROM vk_ads
-        GROUP BY utm_source, utm_medium, utm_campaign, campaign_date
+tab AS (
+    SELECT
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        CAST(campaign_date AS DATE) AS campaign_date,
+        SUM(daily_spent) AS total_cost
+    FROM vk_ads
+    GROUP BY
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        campaign_date
 
-        UNION
+    UNION
 
-        SELECT
-            utm_source,
-            utm_medium,
-            utm_campaign,
-            CAST(campaign_date AS DATE) AS campaign_date,
-            SUM(daily_spent) AS total_cost
-        FROM ya_ads
-        GROUP BY utm_source, utm_medium, utm_campaign, campaign_date
+    SELECT
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        CAST(campaign_date AS DATE) AS campaign_date,
+        SUM(daily_spent) AS total_cost
+    FROM ya_ads
+    GROUP BY
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        campaign_date
 ),
 
-    tab2 AS (
-        SELECT
-            tab1.source,
-            tab1.medium,
-            tab1.campaign,
-            CAST(tab1.visit_date AS DATE) AS visit_date,
-            COUNT(tab1.visitor_id) AS visitors_count,
-            COUNT(tab1.visitor_id) FILTER (WHERE tab1.created_at IS NOT NULL) AS leads_count,
-            COUNT(tab1.visitor_id) FILTER (WHERE tab1.status_id = 142) AS purchases_count,
-            SUM(tab1.amount) FILTER (WHERE tab1.status_id = 142) AS revenue
-        FROM tab1
-        GROUP BY
-            tab1.source,
-            tab1.medium,
-            tab1.campaign,
-            CAST(tab1.visit_date AS DATE)
+tab2 AS (
+    SELECT
+        tab1.source,
+        tab1.medium,
+        tab1.campaign,
+        CAST(tab1.visit_date AS DATE) AS visit_date,
+        COUNT(tab1.visitor_id) AS visitors_count,
+        COUNT(tab1.visitor_id) FILTER (WHERE tab1.created_at IS NOT NULL) AS leads_count,
+        COUNT(tab1.visitor_id) FILTER (WHERE tab1.status_id = 142) AS purchases_count,
+        SUM(tab1.amount) FILTER (WHERE tab1.status_id = 142) AS revenue
+    FROM tab1
+    GROUP BY
+        tab1.source,
+        tab1.medium,
+        tab1.campaign,
+        CAST(tab1.visit_date AS DATE)
 )
 
 SELECT
